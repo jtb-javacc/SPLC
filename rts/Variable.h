6d6659af8826af665422d2fd38eaf7a4df9336ab
FrancisANDRE <zosrothko@orange.fr>
2016-12-28 05:38:11 +0100

    WIP
    Signed-off-by: FrancisANDRE <zosrothko@orange.fr>

---
+++ rts/Variable.h
@@ -0,0 +1,18 @@
+/*
+ * Variable.h
+ *
+ *  Created on: 28 déc. 2016
+ *      Author: FrancisANDRE
+ */
+
+#pragma once
+
+#include "Node.h"
+
+class Variable : public Node {
+public:
+	Variable();
+	virtual ~Variable();
+};
+
+
