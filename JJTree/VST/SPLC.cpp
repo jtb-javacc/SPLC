b952062cc694acf1a6028d043a371547076c6cff
FrancisANDRE <zosrothko@orange.fr>
2017-01-20 08:33:45 +0100

    Repackaging. Add VST for the visitor generation.

--- JJTree/VST/SPLC.cpp
+++ JJTree/VST/SPLC.cpp
@@ -18,19 +18,22 @@ using namespace std;
 #include "JavaCC.h"
 #include "SPLParserTokenManager.h"
 #include "ParseException.h"
+#include "DumpVisitor.h"
 
 JAVACC_STRING_TYPE ReadFileFully(char *file_name) {
 	return "(1 + 2) * (a + b);\n";
 }
 
 int main(int argc, char**argv) {
-	JAVACC_STRING_TYPE s = ReadFileFully(argv[1]);
+	JJString s = ReadFileFully(argv[1]);
 	try {
 		CharStream *stream = new CharStream(s.c_str(), s.size() - 1, 1, 1);
 		SPLParserTokenManager *scanner = new SPLParserTokenManager(stream);
 		SPLParser parser(scanner);
 		parser.CompilationUnit();
 		Node* n = parser.rootNode();
+		DumpVisitor dumpVisitor;
+		n->jjtAccept(&dumpVisitor, nullptr);
 	} catch (const ParseException& e) {
 
 	}
